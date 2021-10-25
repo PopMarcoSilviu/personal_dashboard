8d403dc0780fd5947ccd62269f34f48a58a15c8a
Marco Silviu Pop Mihali <marco_silviu2000@yahoo.com>
2021-10-25 17:32:31 +0300

    first commit

---
+++ api/serializers.py
@@ -0,0 +1,29 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+from rest_framework.validators import UniqueTogetherValidator
+
+
+class UserSerializer(serializers.ModelSerializer):
+
+    def create(self, validated_data):
+        dictionary = {'first_name': validated_data['first_name'],
+                      'last_name': validated_data['last_name'], }
+        user = User.objects.create_user(validated_data['username'], validated_data['email'], validated_data['password'],
+                                        **dictionary)
+        return user
+
+    class Meta:
+        model = User
+        fields = (
+            'username',
+            'first_name',
+            'last_name',
+            'email',
+            'password',
+        )
+        validators = [
+            UniqueTogetherValidator(
+                queryset=User.objects.all(),
+                fields=['username', 'email']
+            )
+        ]
